cd10989813cdbff47cbff191c2d6d7b81a9280c0
Simon Torres PXL <12504946@student.pxl.be>
2026-01-11 15:22:55 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+    document.addEventListener("DOMContentLoaded", () => {
+    const items = document.querySelectorAll(".reveal");
+
+    const observer = new IntersectionObserver(
+    (entries) => {
+    entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+    entry.target.classList.add("show");
+    observer.unobserve(entry.target);
+}
+});
+},
+{ threshold: 0.15 }
+    );
+
+    items.forEach((item) => observer.observe(item));
+});
+    document.addEventListener("DOMContentLoaded", () => {
+    const items = document.querySelectorAll(".reveal");
+
+    const observer = new IntersectionObserver(
+    (entries) => {
+    entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+    entry.target.classList.add("show");
+    observer.unobserve(entry.target);
+}
+});
+},
+{ threshold: 0.15 }
+    );
+
+    items.forEach((item) => observer.observe(item));
+});
+
